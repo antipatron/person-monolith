6ad83196b0687eac6a128d7c8294e5f48b2241ea
QuantumInformatik <juanguillermoalarcon@gmail.com>
2021-12-14 10:31:36 -0500

    First commit

---
+++ src/main/java/com/pragma/personmonolith/PersonMonolithApplication.java
@@ -0,0 +1,13 @@
+package com.pragma.personmonolith;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonMonolithApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersonMonolithApplication.class, args);
+    }
+
+}
